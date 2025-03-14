2c039415f49caba034256f17494df5429f04f304
praneshkrsingh <praneshbfcsofttech@gmail.com>
2025-03-15 01:07:30 +0530

    updated

--- src/app/performance-optimization/page.jsx
+++ src/app/performance-optimization/page.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Link from "next/link";
 import ControlledTabsExample from "../components/DataEngineeringTab";
-import "./page.css";
 
 const Page = () => {
   return (
