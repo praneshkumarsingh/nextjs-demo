1d04190c12957ad05a57a22a4e316ef1b185ab14
praneshkrsingh <praneshbfcsofttech@gmail.com>
2025-03-16 19:31:41 +0530

    we

--- src/app/service-details/page.jsx
+++ src/app/service-details/page.jsx
@@ -29,62 +29,6 @@ const ServiceDetail = () => {
         </div>
       </section>
 
-      <section className="service-about section-gap">
-        <div className="container">
-          <div className="row">
-            <div className="col-xl-4">
-              <div className="choose-us-style-two">
-                <h2 className="title mb-5">
-                  Grow business <br /> with creative <br /> ideas
-                </h2>
-                <ul className="check-list-item">
-                  <li>
-                    <h4>Professional Agency</h4>
-                    <p>
-                      Consectetur adipisci velitsed quia non numquam eius
-                      tempralabore et dolore magnam aliquam quaerat seperate.
-                    </p>
-                  </li>
-                  <li>
-                    <h4>Solutions Provider</h4>
-                    <p>
-                      Know more about digital direct response than virtually any
-                      digital marketing agency in the industry same as
-                      distribution.
-                    </p>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div className="col-xl-7 offset-xl-1 text-end">
-              <div className="choose-us-style-two-thumb">
-                <div className="curve-text">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 150 150"
-                    version="1.1"
-                  >
-                    <path id="textPath" d="M 0,75 a 75,75 0 1,1 0,1 z"></path>
-                    <text>
-                      <textPath href="#textPath">
-                        Award winning creative digital agency
-                      </textPath>
-                    </text>
-                  </svg>
-                </div>
-                <h4>We have worked for you</h4>
-                <h2 className="text-path">since 1980</h2>
-                <img
-                  src="/img/about-service.jpg"
-                  alt="Image Not Found"
-                  className="img-fluid"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-
       <section className="whyChooseSec section-gap mb-5">
         <div className="container">
           <div className="row">
