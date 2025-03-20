ccf052580976a94aa9b2bdc20a77f3b5f0f9f394
praneshkrsingh <praneshbfcsofttech@gmail.com>
2025-03-21 00:31:22 +0530

    up

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -31,9 +31,7 @@ const page = () => {
           <div className="row justify-content-center">
             <div className="col-xxl-5 col-xl-6">
               <div className="inner_section-area mb-50 text-center">
-                <h2 className="title mb-5">
-                  Engage in conversation with skilled engineers.
-                </h2>
+                <h2 className="title mb-5">No challenge, too big for us!</h2>
               </div>
             </div>
           </div>
@@ -48,8 +46,11 @@ const page = () => {
                     <i className="fa-solid fa-message"></i>
                   </div>
                   <div className="contact-content">
-                    <h4 className="contact-content-title">Chat with us.</h4>
-                    <p>Monday - Friday : 9am to 6pm.</p>
+                    <h4 className="contact-content-title">Let's meet</h4>
+                    <p>
+                      Monday - Friday | 10 AM - 5 PM <br />
+                      Abu Dhabi Office
+                    </p>
                   </div>
                 </div>
               </div>
@@ -63,9 +64,9 @@ const page = () => {
                     <i className="fa-solid fa-phone-volume"></i>
                   </div>
                   <div className="contact-content">
-                    <h4 className="contact-content-title">Give us a call</h4>
-                    <a href="tel:+480-555-0103">+480-555-0103</a>
-                    <a href="tel:+239-555-0108">+239-555-0108</a>
+                    <h4 className="contact-content-title">Talk to us!</h4>
+                    <a href="tel:+16174707070">+1 617.470.7070</a>
+                    {/* <a href="tel:+239-555-0108">+239-555-0108</a> */}
                   </div>
                 </div>
               </div>
@@ -79,12 +80,10 @@ const page = () => {
                     <i className="fa-solid fa-envelope-open"></i>
                   </div>
                   <div className="contact-content">
-                    <h4 className="contact-content-title">Email with us.</h4>
-                    <a href="mailto:support.young@example.com">
-                      support.young@example.com
-                    </a>
-                    <a href="mailto:bill.robert@example.com">
-                      bill.robert@example.com
+                    <h4 className="contact-content-title">Write to us</h4>
+                    <a href="mailto:info@elile.ai ">info@elile.ai</a>
+                    <a href="mailto:investinenergy@elile.ai">
+                      investinenergy@elile.ai
                     </a>
                   </div>
                 </div>
@@ -97,13 +96,14 @@ const page = () => {
                 <div className="contact-bottom-left">
                   <div className="inner_section-area mb-50">
                     <h2 className="title mb-4">
-                      Connect with & ignite <br /> the conversation!
+                      Connect to explore
+                      <br /> advanced AI solutions!
                     </h2>
                   </div>
                   <div className="contact-map tp_fade_right">
                     <div className="faq-content">
                       <div className="accordion" id="Expp">
-                        <div className="accordion-item mb-30">
+                        <div className="accordion-item mb-4">
                           <h2 className="accordion-header" id="headingOne">
                             <button
                               className="accordion-button py-3"
@@ -123,18 +123,16 @@ const page = () => {
                             data-bs-parent="#Expp"
                           >
                             <div className="accordion-body p-3">
-                              <iframe
-                                src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d14599.593481274613!2d90.42342665!3d23.8222127!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1701453167946!5m2!1sen!2sbd"
-                                width="600"
-                                height="250"
-                                allowFullScreen=""
-                                loading="lazy"
-                                referrerPolicy="no-referrer-when-downgrade"
-                              ></iframe>
+                              <p>
+                                ElectrOS AI- FZCO, Dubai Technology Entrepreneur
+                                Campus (DTEC), PO BOX # 341173, A5 Building,
+                                Dubai Digital Park, Dubai Silicon Oasis, Dubai,
+                                United Arab Emirates
+                              </p>
                             </div>
                           </div>
                         </div>
-                        <div className="accordion-item mb-30">
+                        <div className="accordion-item mb-4">
                           <h2 className="accordion-header" id="headingTwo">
                             <button
                               className="accordion-button collapsed py-3"
@@ -154,18 +152,15 @@ const page = () => {
                             data-bs-parent="#Expp"
                           >
                             <div className="accordion-body p-3">
-                              <iframe
-                                src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d14599.593481274613!2d90.42342665!3d23.8222127!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1701453167946!5m2!1sen!2sbd"
-                                width="600"
-                                height="450"
-                                allowFullScreen=""
-                                loading="lazy"
-                                referrerPolicy="no-referrer-when-downgrade"
-                              ></iframe>
+                              <p>
+                                ELILE SOFTWARE TECHNOLOGIES - L.L.C - S.P.C, Al
+                                Hisn W7_01, First Abu Dhabi Bank Building, Abu
+                                Dhabi Municipality, Abu Dhabi
+                              </p>
                             </div>
                           </div>
                         </div>
-                        <div className="accordion-item mb-30">
+                        <div className="accordion-item mb-4">
                           <h2 className="accordion-header" id="headingThree">
                             <button
                               className="accordion-button collapsed py-3"
@@ -185,8 +180,12 @@ const page = () => {
                             data-bs-parent="#Expp"
                           >
                             <div className="accordion-body p-3">
+                              <p className="mb-3">
+                                Elile AI Corporation, 2331 130th Ave NE, Suite
+                                110-M, Bellevue WA 98005
+                              </p>
                               <iframe
-                                src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d14599.593481274613!2d90.42342665!3d23.8222127!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1701453167946!5m2!1sen!2sbd"
+                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d21516.63841179046!2d-122.18972318639415!3d47.61485973617184!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54906c1b7ab8536b%3A0x5ce49d96bdecadc5!2sAUE!5e0!3m2!1sen!2sin!4v1742496714287!5m2!1sen!2sin"
                                 width="600"
                                 height="450"
                                 allowFullScreen=""
@@ -216,13 +215,17 @@ const page = () => {
                             data-bs-parent="#Expp"
                           >
                             <div className="accordion-body p-3">
+                              <p className="mb-3">
+                                Elile AI Corporation, 251 Little Falls Drive,
+                                Wilmington, DE 19808
+                              </p>
+
                               <iframe
-                                src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d14599.593481274613!2d90.42342665!3d23.8222127!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1701453167946!5m2!1sen!2sbd"
+                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3067.0421917003673!2d-75.6254295245508!3d39.76116109527945!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c6fe0b3ff36ac7%3A0x55af8d24780e66d6!2sCSC%20Headquarters!5e0!3m2!1sen!2sin!4v1742496496534!5m2!1sen!2sin"
                                 width="600"
                                 height="450"
                                 allowFullScreen=""
                                 loading="lazy"
-                                referrerPolicy="no-referrer-when-downgrade"
                               ></iframe>
                             </div>
                           </div>
@@ -234,7 +237,9 @@ const page = () => {
               </div>
               <div className="col-lg-6 tp_fade_left">
                 <div className="contact-form mt-40 mt-lg-0">
-                  <h3 className="contact-form-title">Fillup the form</h3>
+                  <h3 className="contact-form-title">
+                    Please fill out the form!
+                  </h3>
                   <form action="#">
                     <div className="row">
                       <div className="col-xl-6 col-lg-12 col-md-6">
